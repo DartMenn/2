39e0ce33bb8b68445756facbe3fe8966037980c2
DartMenn <megor643@gmail.com>
2024-03-24 13:54:46 +0300

    3
    3

---
+++ Assets/Assets/Scripts/Explosion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float maxSize = 5;
+    public float speed = 5;
+
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+    private void Update()
+    {
+        transform.localScale += Vector3.one * Time.deltaTime * speed;
+
+        if (transform.localScale.x > maxSize)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
